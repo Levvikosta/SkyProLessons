fb962ce5b21c35e4a5509914944bdd399a507800
Volkova Tatyana <levikosta2015@gmail.com>
2024-11-28 17:38:41 +0300

    change branch

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+from typing import Iterable, Any
+
+
+def filter_by_state(dict_list: Iterable[list[dict[Any, Any]]], state: Any='EXECUTED')-> list[list[dict[Any, Any]]]:
+    """Функция для выведения данных по значению"""
+    executed_list =[]
+    for i in dict_list:
+        if i['state'] == state:
+            executed_list.append(i)
+    return executed_list
+
+def sort_by_date(dictionaries: Iterable[list[dict[Any, Any]]], reverse: bool =True) -> list[list[dict[Any, Any]]]:
+    """Функция для сортировки по датам"""
+    list_by_date = sorted(dictionaries, key = lambda x: x['date'], reverse=reverse)
+    return list_by_date
+
+
