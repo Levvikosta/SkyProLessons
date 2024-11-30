5b75d01d798564140646822f2200cba79b3ac55d
Levvikosta <levikosta2015@gmail.com>
2024-11-30 10:41:10 +0300

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -3,11 +3,12 @@ from typing import Iterable, Any, List, Dict
 
 def filter_by_state(dict_list: Iterable[list[dict[Any, Any]]], state: Any='EXECUTED')-> list[list[dict[Any, Any]]]:
     '''Функция для выведения данных по значению'''
-    executed_list =[]
+    executed_list = []
     for i in dict_list:
         if i['state'] == state:
             executed_list.append(i)
     return executed_list
+
 
 def sort_by_date(dictionaries: Iterable[list[dict[Any, Any]]], reverse: bool =True) -> list[list[dict[Any, Any]]]:
     '''Функция для сортировки по датам'''
